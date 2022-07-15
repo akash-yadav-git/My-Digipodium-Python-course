c29cb6e75e24a2eaf4921e89e60a5ee89fc0799b
akash-yadav-git <akashyadavreigns@gmail.com>
2022-07-15 17:00:44 +0530

    turtle

---
+++ Data Structure/tlist.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed('fastest')
+colors = ['red','blue','green','black']
+s=len(colors)
+for i in range(500):
+ 
+    c=colors[i % s]
+    pencolor(c)
+    forward(i+5)
+    left(360/s)
+    for j in range(s):
+        forward(100)
+        left(60)
+mainloop()
